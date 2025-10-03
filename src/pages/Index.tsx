34bdf225088bacde58878c1737124de29bc29412
[dyad] <git@dyad.sh>
2025-10-03 17:08:26 +0400

    [dyad] Integrated HTML OS simulation as main app content - wrote 1 file(s), deleted 13 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import React from 'react';
-import OSDesktop from "@/components/os/OSDesktop";
+import OSSimulator from "@/components/OSSimulator";
 
 const Index = () => {
   return (
     <div className="w-full h-screen">
-      <OSDesktop />
+      <OSSimulator />
     </div>
   );
 };
